374060a5fdfc7fd36457b55f445ec22c576b6d77
Dtungdeptry <162943193+Dtungdeptry@users.noreply.github.com>
2024-04-05 00:24:28 +0700

    Update TestPhongBan.java

--- QuanLyNhanSu/src/QuanLyPhongBan/TestPhongBan.java
+++ QuanLyNhanSu/src/QuanLyPhongBan/TestPhongBan.java
@@ -5,5 +5,7 @@ import QuanLyPhongBan.View.PhongBanView;
 public class TestPhongBan {
     public static void main(String[] args) {
         PhongBanView phongBanView = new PhongBanView();
+    
+    //Thêm idPhongBan 7 là Khác
     }
 }
